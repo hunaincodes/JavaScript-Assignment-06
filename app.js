c24a69102218b80f8b9b642d3f1256dd251304fd
Muhammad Hunain <hunainamir76@gmail.com>
2025-12-25 18:43:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+// Q no 1
+
+// var firstName = prompt ("Enter your First name");
+
+// var lastName = prompt ("Enter your last name");
+
+// var fullName = firstName + " " + lastName;
+
+// console.log("Hello " + fullName);
+
+
+// Q no 2
+
+
+//  var mobileModel = prompt ("Enter your favorite mobile model");
+
+//  var lenght = mobileModel.length;
+
+//  document.write("My favorite phone is: " + mobileModel + "<br>");
+//  document.write("Lenght of string: " + lenght); 
+
+
+// var mobileModel = prompt("Enter your favorite mobile model");
+
+// var length = mobileModel.length;
+
+// document.write("My favorite mobile model is: "  + mobileModel + "<br>");
+// document.write("length of string: " + length)
+
+
+
+// Q no 3
+
+// var word = "Pakistani";
+
+// var index = word.indexOf("n");
+
+// document.write("Index of letter n is: " + index);
+
+
+// var car = "Mercedes";
+
+// var num = car.indexOf("e");
+
+// document.write("Index of letter e is: " + num);
+
+
+// Q no 4
+
+// var word = "Hello World";
+
+// var index = word.lastIndexOf("l");
+
+// document.write("Index of letter l is: " + index);
+
+
+// Q no 5
+
+// var word = "Pakistani";
+
+// var index = word.charAt(3);
+
+// document.write("index of 3rd letter is: " + index);
+
+
+// Q no 6
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+
+// var fullName = firstName + " " + lastName;
+
+// alert("Hello " + fullName);
+
+
+// var mobile = prompt("Enter your favorite mobile phone model");
+// var length = mobile.length;
+
+// document.write("My favorite phone is: " + mobile + "<br>");
+// document.write("Length of string: " + length);
+
+
+// Q no 7 
+
+// var city = "Hyderabad";
+
+// var newCity = city.replace("Hyder", "Islam")
+
+// document.write("city: " + newCity);
+
+
+
+// Q no 8
+
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+
+// var updatedMessage = message.replace(/and/g, "&");
+
+// document.write(updatedMessage);
+
+// Q no 9
+
+
+// var str = "472";
+
+ 
+// var num = Number(str);
+
+// document.write("Value of str: " + str + "<br>");
+// document.write("Value of num: " + num + "<br><br>");
+
+// document.write("Type of str: " + typeof str + "<br>");
+// document.write("Type of num: " + typeof num);
